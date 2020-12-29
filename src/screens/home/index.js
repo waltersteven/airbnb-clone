4de803cd87963a1e581a217771eae5042635679a
Walter Pariona <wltrst97@gmail.com>
2020-12-29 00:46:09 -0500

    Added home screen

---
+++ src/screens/home/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {View, Text, ImageBackground, Pressable} from 'react-native';
+import styles from './styles';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Pressable
+        style={styles.searchButton}
+        onPress={() => console.warn('Search button was clicked!')}>
+        <Fontisto name="search" size={25} color={'#f15454'} />
+        <Text style={styles.searchButtonText}>Where are you going?</Text>
+      </Pressable>
+      <ImageBackground
+        source={require('../../../assets/images/wallpaper.jpg')}
+        style={styles.image}>
+        <Text style={styles.title}>Go Near</Text>
+        <Pressable
+          style={styles.button}
+          onPress={() => console.warn('Button was clicked!')}>
+          <Text style={styles.buttonText}>Explore nearby stays</Text>
+        </Pressable>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default HomeScreen;
