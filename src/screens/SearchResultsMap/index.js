edccd6988d739d9020eedc026d2c148680469531
Walter Pariona <wltrst97@gmail.com>
2021-03-02 23:14:38 -0500

    Added behavior to sync carousel items with map

--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {View, Text, FlatList, useWindowDimensions} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import CustomMapMarker from '../../components/CustomMapMarker/index';
@@ -8,12 +8,42 @@ import CarouselItem from '../../components/Post/CarouselItem';
 
 const SearchResultsMap = () => {
   const [selectedPlace, setSelectedPlace] = useState(null);
-
   const width = useWindowDimensions().width;
+
+  const flatList = useRef();
+  const map = useRef();
+
+  const viewConfig = useRef({itemVisiblePercentThreshold: 70});
+  const onViewChanged = useRef(({viewableItems}) => {
+    if (viewableItems.length > 0) {
+      const selectedItem = viewableItems[0].item;
+      setSelectedPlace(selectedItem.id);
+    }
+  });
+
+  useEffect(() => {
+    if (!selectedPlace || !flatList) {
+      return;
+    }
+
+    const index = places.findIndex((place) => place.id === selectedPlace);
+    flatList.current.scrollToIndex({index});
+
+    const selectedItem = places[index];
+    const region = {
+      latitude: selectedItem.coordinate.latitude,
+      longitude: selectedItem.coordinate.longitude,
+      latitudeDelta: 0.8,
+      longitudeDelta: 0.8,
+    };
+
+    map.current.animateToRegion(region);
+  }, [selectedPlace]);
 
   return (
     <View style={{width: '100%', height: '100%'}}>
       <MapView
+        ref={map}
         provider={PROVIDER_GOOGLE}
         style={{width: '100%', height: '100%'}}
         initialRegion={{
@@ -36,6 +66,7 @@ const SearchResultsMap = () => {
       <View style={{position: 'absolute', bottom: 40}}>
         {/* <CarouselItem post={places[0]} /> */}
         <FlatList
+          ref={flatList}
           data={places}
           renderItem={({item}) => <CarouselItem post={item} />}
           horizontal
@@ -43,6 +74,8 @@ const SearchResultsMap = () => {
           snapToInterval={width - 60}
           snapToAlignment={'center'}
           decelerationRate={'fast'}
+          viewabilityConfig={viewConfig.current}
+          onViewableItemsChanged={onViewChanged.current}
         />
       </View>
     </View>
